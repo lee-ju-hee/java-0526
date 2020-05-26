e88e4ed1c4408682b982f88abdc12e9d0757a47e
lee-ju-hee <55524173+lee-ju-hee@users.noreply.github.com>
2020-05-26 15:47:56 +0900

    Add files via upload

--- TestApp.java
+++ TestApp.java
@@ -1,22 +1,16 @@
-package java02;
+package java03;
 
 import java.util.Scanner;
 
 public class TestApp {
 
-//	static int kor1;
-//	static int kor2;
-//	static int kor3;
-//	static int total;
-//	static double avg;
-
 	public static void main(String[] args) {
 
-		int[] kors = new int[3];
+		Exam[] exams = new Exam[3];
+
+		// exam. -> 연산자
 		int menu = 0;
 		boolean flag = true;
-
-		Scanner scan = new Scanner(System.in);
 
 		while (flag) {
 
@@ -24,11 +18,11 @@ public class TestApp {
 
 			switch (menu) {
 			case 1:
-				inputScore(kors);
+				inputScore(exams);
 				break;
 
 			case 2:
-				displayScore(kors);
+				displayScore(exams);
 				break;
 
 			case 3:
@@ -39,27 +33,35 @@ public class TestApp {
 		System.out.println("종료합니다.");
 	}
 
-	private static void displayScore(int[] kors) {
+	private static void displayScore(Exam[] exams) {
 		// 출력
 		int total = 0;
 		double avg = 0.0;
-		for (int i = 0; i < 3; i++)
-			total += kors[i];
-		avg = total / 3;
-		System.out.println("┌──────────┐");
-		System.out.println("│  성적 출력   │");
-		System.out.println("└──────────┘");
 
-		for (int i = 0; i < 3; i++)
-			System.out.printf("국어%d: %d\n", i + 1, kors[i]);
+		for (int i = 0; i < 3; i++) {
+			Exam exam = exams[i];
+			int kor = exam.kor;
+			int eng = exam.eng;
+			int math = exam.math;
 
-		System.out.printf("총점: %d\n", total);
-		System.out.printf("평균: %6.2f\n", avg);// 전체 6자리 중 소수점 2번째까지
+			total = kor + eng + math;
+			avg = total / 3;
+			System.out.println("┌──────────┐");
+			System.out.println("│  성적 출력   │");
+			System.out.println("└──────────┘");
 
-		System.out.println("------------------");
+			System.out.printf("국어: %d\n", kor);
+			System.out.printf("영어: %d\n", eng);
+			System.out.printf("수학: %d\n", math);
+
+			System.out.printf("총점: %d\n", total);
+			System.out.printf("평균: %6.2f\n", avg);// 전체 6자리 중 소수점 2번째까지
+
+			System.out.println("------------------");
+		}
 	}
 
-	private static void inputScore(int[] kors) {
+	private static void inputScore(Exam[] exams) {
 
 		Scanner scan = new Scanner(System.in);
 		// 입력
@@ -67,15 +69,39 @@ public class TestApp {
 		System.out.println("│  성적 입력   │");
 		System.out.println("└──────────┘");
 
-		for (int i = 0; i < 3; i++)
-			do {
-				System.out.printf("국어%d: ", i + 1);
-				kors[i] = scan.nextInt();
-				if (kors[i] < 0 || 100 < kors[i])
-					System.out.println("성적범위를 (0~100) 벗어났습니다.");
-			} while (kors[i] < 0 || kors[i] > 100);
+		for (int i = 0; i < 3; i++) {
+			int kor, eng, math;
 
-		System.out.println("------------------");
+			do {
+				System.out.print("국어: ");
+				kor = scan.nextInt();
+				if (kor < 0 || 100 < kor)
+					System.out.println("성적범위를 (0~100) 벗어났습니다.");
+			} while (kor < 0 || kor > 100);
+
+			do {
+				System.out.print("영어: ");
+				eng = scan.nextInt();
+				if (eng < 0 || 100 < eng)
+					System.out.println("성적범위를 (0~100) 벗어났습니다.");
+			} while (eng < 0 || eng > 100);
+
+			do {
+				System.out.print("수학: ");
+				math = scan.nextInt();
+				if (math < 0 || 100 < math)
+					System.out.println("성적범위를 (0~100) 벗어났습니다.");
+			} while (math < 0 || math > 100);
+
+			Exam exam = new Exam();
+			exam.kor = kor;
+			exam.eng = eng;
+			exam.math = math;
+
+			exams[i] = exam;
+
+			System.out.println("------------------");
+		}
 
 	}
 
